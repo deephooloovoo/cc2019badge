ce43dbcc2bf46b63a376fb69ca6e6e44a72785f1
fortytwopar <paul.emge@gmail.com>
2019-04-20 14:46:05 -0500

    add pdfs containing all the tapes, and make them pretty

--- makejob.py
+++ makejob.py
@@ -2,12 +2,6 @@
 import sys
 HEAD="0102"
 END="0319"
-bodies = [
-    #"ADD1EACCE55EDA5C11",
-    "5afe11fe512e501ace".upper(),
-    "C0DED15C105ED10CA1",
-    "A11CEDD05EDB0BC01D"
-    ]
         
 def openuser(userfn):
     lines=[]
@@ -16,23 +10,37 @@ def openuser(userfn):
         for a in l:
             lines.append(a.strip())
     if len(lines[0])<13:
-        lines[2] += (13-len(lines[2]))*" "
+        lines[0] += (13-len(lines[0]))*" "
         
-    return (lines[0], lines[1], lines[2])
+    return (lines[0], lines[1])
+
+def openbody(userfn):
+    lines=[]
+    with open(userfn) as f:
+        l = f.readlines()
+        for a in l:
+            sline = a.strip().split(" ")
+            gid = int(sline[1],16)
+            gid = "{:02x}".format(gid)
+            
+            lines.append((sline[0],gid))
+    return lines
         
 
-def makeflag(uid,bn,auid, body ):
+def makeflag(uid,bn,gid, body ):
     assert len(body) == 18
-    return "0102"+uid+bn+auid+body+"0319"
+    return "0102"+gid+bn+uid+body+"0319"
         
 
 
 def main():
     lines =[]
     uf = sys.argv[1]
-    u,bid,a = openuser(uf)
-    for b in bodies:
-        f = makeflag(u,bid,a,b)
+    bf = sys.argv[2]
+    u,bid = openuser(uf)
+    bodies = openbody(bf)
+    for b,gid in bodies:
+        f = makeflag(u,bid,gid,b)
         print(f)
     
 if __name__=="__main__":
--- mktape.py
+++ mktape.py
@@ -10,8 +10,9 @@ MARGINY=.25
 SPACING= .1
 OVERLAP= 1
 SHEET_W =8
-SHEET_H =10.5
+SHEET_H =6.5
 STRIPL =SHEET_H
+TAPEW = .98
 
 
 def onsheet(px,py):
@@ -27,37 +28,44 @@ def drawhole(ctx,x,y,r):
     ctx.arc(x,y,r,0.0,2*math.pi)
     ctx.close_path()
     ctx.fill()
-def drawstrip(ctx,d,col):
+def drawstrip(ctx,t,col):
     #ctx.rectangle(0,0,MARGINX,MARGINY)
+    d,txt = t
     ctx.set_source_rgb(0,0,0)
     #ctx.set_source_rgb(0,0,0)
     #ctx.fill()
     #for s in range(1):
     s = col
     print(s)
-    if 1:
         
-        ctx.rectangle(s * 1.0+MARGINX, 0+MARGINY, 1.0,SHEET_H)
-        ctx.set_source_rgb(0,0,0)
-        #ctx.set_source_rgb(0,0,0)
-        ctx.fill()
-        ctx.rectangle(s * 1.0+MARGINX, 0+MARGINY, 1.0,SHEET_H)
-        ctx.set_source_rgb(1,1,1)
-        ctx.set_line_width(.005)
-        ctx.stroke()
-        for n in range(len(d)):
-            drawbyte(ctx,d,n,s)
+    ctx.rectangle((s%8) * TAPEW+MARGINX, 0+MARGINY, TAPEW,SHEET_H)
+    ctx.set_source_rgb(0,0,0)
+    #ctx.set_source_rgb(0,0,0)
+    ctx.fill()
+    ctx.rectangle((s%8) * TAPEW+MARGINX, 0+MARGINY, TAPEW,SHEET_H)
+    ctx.set_source_rgb(1,1,1)
+    ctx.set_line_width(.005)
+    ctx.stroke()
+    for n in range(len(d)):
+        drawbyte(ctx,d,n,s%8)
+    ctx.set_source_rgb(0,0,0)
+    ctx.move_to(TAPEW*(s%8+.5)-.1+MARGINX,STRIPL+MARGINY+.1)
+    ctx.set_font_size(.10)
+    ctx.save()
+    ctx.rotate(3.14/2)
+    ctx.show_text("{:02d}     ".format(s)+txt)
+    ctx.restore()
         
 def drawbyte(ctx,d, n, strip):
     #print(n,strip)
     py = SPACING*n+OVERLAP +MARGINY
     #-(STRIPL - OVERLAP) * strip + MARGINY
     for a in range(8):
-        px = HOLE0X + SPACING*(a +(a>=3)) + strip + MARGINX
+        px = HOLE0X + SPACING*(a +(a>=3)) + strip*TAPEW + MARGINX
         if (ord(d[n])>>a)&1:
             if(onsheet(px,py)):
                 drawhole(ctx,px,py,DHOLER)
-    px = HOLE0X + SPACING*(3)+strip + MARGINX
+    px = HOLE0X + SPACING*(3)+strip*TAPEW + MARGINX
     if(onsheet(px,py)):
         drawhole(ctx,px,py,FHOLER)
 def maketape(d,fn):
@@ -66,9 +74,11 @@ def maketape(d,fn):
     # surf = cairo.PSSurface(fn,72*8.5,72*11)
     ctx = cairo.Context(surf)
     ctx.scale(72,72) # work in inches, because that's how tape specs are
+    ctx.select_font_face("mono")
     for q in range(len(d)):
-        a = "\x00" * 5 + d[q].strip() + "\x0d\x0a" + "\x00" * 5
-        drawstrip(ctx,a,q)
+        if (q and (q%8) == 0):
+            ctx.show_page()
+        drawstrip(ctx,d[q],q)
 def parity(a):
     p=0
     for b in range(7):
@@ -86,8 +96,9 @@ def main():
         d = f.readlines()
         o = []
         for l in d:
-            f = "".join(fixparity(a) for a in l)
-            o.append(f)
+            a = "\x00" * 2 + l.strip() + "\x00" * 2
+            f = "".join(fixparity(b) for b in a)
+            o.append((f,l.strip()))
            
 
         maketape(o,ofn)
